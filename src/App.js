5f7c6b5de865ce736f6fd0d005cfe61d8997b8ab
Niranjan-guru <niranjanbalaguru63@gmail.com>
2024-07-02 14:48:30 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -26,6 +26,32 @@ function App() {
     setCart([]);
   };
 
+  return(
+    <div className="App">
+      <header className='App-header'>
+        <h1> Commerce Store </h1>
+      </header>
+      <section className='App-products'>
+        {products.map(product => {
+          <div key={product.id} className='App-product'>
+            <img src={product.image} alt={product.name} style={{width: '100%' , height: 'auto'}} />
+            <h3> {product.name} </h3>
+            <p> ${product.price} </p>
+            <button onClick={() => addTocart(product.id)}>Add to Cart</button>
+          </div>
+        })}
+      </section>
+      <section className='App-cart'>
+        <h2>Shopping Cart</h2>
+        <ul>
+          {cart.map(item => (
+            <li key={item.id}>{item.name} - ${item.price}</li>
+          ))}
+        </ul>
+        <button onClick={checkOut}>CheckOut</button>
+      </section>
+    </div>
+  );
 }
 
 export default App;

a260d7a0acb409f37064e3ef2e9dd42a61bc5365
Niranjan-guru <niranjanbalaguru63@gmail.com>
2024-07-02 13:07:55 +0530

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';  
+import chinos from "./images/chinos.jpg";
+import shirts from "./images/shirts.jpg";
+import shoes from "./images/shoes.jpg";
 import './App.css';
+
+const products = [
+  {id: 1, name: 'DENIM', price: 10, image: chinos},
+  {id: 2, name: 'AMERICAN POLO', price: 9.30, image: shirts},
+  {id: 3, name: 'NIKE', price: 35, image: shoes},
+  
+];
 
 function App() {
   return (

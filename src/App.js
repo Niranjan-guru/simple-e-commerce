86ecca86b6802c90ed3ef1137287675f806941e7
Niranjan-guru <niranjanbalaguru63@gmail.com>
2024-07-02 13:09:54 +0530

    Made Changes

--- src/App.js
+++ src/App.js
@@ -8,28 +8,24 @@ const products = [
   {id: 1, name: 'DENIM', price: 10, image: chinos},
   {id: 2, name: 'AMERICAN POLO', price: 9.30, image: shirts},
   {id: 3, name: 'NIKE', price: 35, image: shoes},
-  
+
 ];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cart, setCart] = useState([]);
+
+  const addTocart = (productId) => {
+    const selectedProduct = products.find(product => product.id === productId);
+    if (selectedProduct){
+      setCart([...cart, selectedProduct]);
+    }
+  };
+
+  const checkOut = () =>{
+    alert("Thank you for purchasing!");
+    setCart([]);
+  };
+
 }
 
 export default App;
